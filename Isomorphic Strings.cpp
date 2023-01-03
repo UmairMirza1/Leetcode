196f7ff0a3c545d82154a49cbe597f24f517bd09
UmairMirza1 <umair3215@gmail.com>
2023-01-03 19:05:27 +0500

    Fixed Testcase

--- Isomorphic Strings.cpp
+++ Isomorphic Strings.cpp
@@ -5,17 +5,23 @@ using namespace std;
 class Solution
 {
 public:
-    bool isIsomorphic(string s, string t)
+   
+     bool isIsomorphic(string s, string t)
     {
         std::map<char, char> mymap;
-
+        set<char> checkedChars;
         for (int i = 0; i < s.length(); i++)
         {
             char c1 = s[i];
             char c2 = t[i];
+           
             if (mymap.find(c1) == mymap.end())
-            {
+            {   
+                if (checkedChars.find(c2) != checkedChars.end())
+				return false;
                 mymap[c1] = c2;
+                checkedChars.insert(c2);
+
             }
             else
             {
@@ -23,9 +29,12 @@ public:
                 {
                     return false;
                 }
+                
             }
+
+           
         }
-        return true;
+         return true;
     }
 };
 
